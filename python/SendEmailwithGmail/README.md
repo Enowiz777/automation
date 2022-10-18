d5559ece595388c7983b4830c81c2db56cf19754
Enoch Park <enoch.park@enowiz.com>
2022-10-17 20:53:21 -0700

    20221017

--- python/SendEmailwithGmail/README.md
+++ python/SendEmailwithGmail/README.md
@@ -5,5 +5,14 @@
 3. Select App > Other > Name Python
 4. Copy the generated password.
 (view main.py code & comments)
+5. Set up Jinja
+```
+pip install Jinja2
+```
+
 
 # Library used
+
+Jinja2
+- File System Loader
+https://jinja.palletsprojects.com/en/3.0.x/api/
--- python/SendEmailwithGmail/main.py
+++ python/SendEmailwithGmail/main.py
@@ -3,27 +3,37 @@ from email.message import EmailMessage
 from app2 import password
 import ssl
 import smtplib
+from jinja2 import Environment, FileSystemLoader, PackageLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader("email-templates-master"),
+    autoescape=select_autoescape(["html", "xml"]),
+)
+
+template = env.get_template("1/index.html")
+html = template.render()
+
 
 # Predefined variables.
-email_sender = 'enowiz89@gmail.com'
+email_sender = "enowiz89@gmail.com"
 # Import from app2.py and mask it when I upload in a github.
 email_password = password
-email_receiver = 'enochpark89@gmail.com'
+email_receiver = "enochpark89@gmail.com"
 subject = "Don't forget to subscribe"
 body = """
-When you watch a video, please hit subscribe.
+# When you watch a video, please hit subscribe.
 """
 
 # Create an email message
 em = EmailMessage()
-em['From'] = email_sender
-em['To'] = email_receiver
-em['subject'] = subject
-em.set_content(body)
+em["From"] = email_sender
+em["To"] = email_receiver
+em["subject"] = subject
+em.set_content(html, subtype="html")
 
 # Create a SSL context
 context = ssl.create_default_context()
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
     smtp.sendmail(email_sender, email_receiver, em.as_string())
 
